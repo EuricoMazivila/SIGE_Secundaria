e76fffa10ea08e4b0c62befd457df9e41f0eb5f6
Paulo Mondlane <paulomondlane55@gmail.com>
2019-08-08 07:19:10 +0200

    Correccao de alguns erros de interface e javascript/jquery

--- Codigo_Juncao_Candido_E_Paulo/_js/TestandoDisciplina.js
+++ Codigo_Juncao_Candido_E_Paulo/_js/TestandoDisciplina.js
@@ -2,9 +2,15 @@
 	$.fn.styleddropdown = function(){
 		return this.each(function(){
 			var obj = $(this)
-			$('td').click(function() { //onclick event, 'list' fadein
+			var conta = 1;
+
+			$( 'td' ).click(function( event ) {
 			obj.find('.list').fadeIn(400);
-			
+
+  				obj.css({
+					'left':(event.pageX-700)+'px',
+					'top':(event.pageY-200)+'px'});
+					
 			$(document).keyup(function(event) { //keypress event, fadeout on 'escape'
 				if(event.keyCode == 27) {
 				obj.find('.list').fadeOut(400);
@@ -14,17 +20,8 @@
 			obj.find('.list').hover(function(){ },
 				function(){
 					$(this).fadeOut(400);
-				});
 			});
-			
-			obj.find('.list li').click(function() { //onclick event, change field value with selected 'list' item and fadeout 'list'
-			$('td')
-				.val($(this).html())
-				.css({
-					'background':'#fff',
-					'color':'#333'
-				});
-			obj.find('.list').fadeOut(400);
+
 			});
 		});
 	};
