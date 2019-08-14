e47657713535324740caa602c39852c8a3236f48
Paulo Mondlane <paulomondlane55@gmail.com>
2019-08-14 21:47:33 +0200

    Ultima versao corrigida. Os comentarios da versao anterior ainda sao validos

--- Codigo/exemplo.php
+++ Codigo/exemplo.php
@@ -2,10 +2,9 @@
     $titulo='Menu Candidato'; //esse e o titulo
     $metadados='metadados.php';//esse e o metadados
     $menu='main_menu_Secretaria.php';//esse e o menu
-    $corpo='Corpo/corpo_candidato.php';//esse e o corpo
+    $corpo='corpo_candidato.php';//esse e o corpo
     $navBar='navbar.php';//essa e o nav bar
     $rodape='footer.php';//especificar a url do footer
     $scriptAdd='scripts_add_candidato.php';
-    //Aqui pode ficar as dependencias de hader como verificar se ja fez login
     include('Templete/Templete.php');
 ?>
