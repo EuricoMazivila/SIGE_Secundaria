d80aa315a09fddb45987557dd6a7fa49a6fbe78f
Eurico Inacio Mazivila <euricoinaciomazive@gmail.com>
2019-11-22 07:45:12 +0200

    Adicionando novos usuarios

--- Sige_Laravel/app/Http/Controllers/Auth/RegisterController.php
+++ Sige_Laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'id_user'=> '2019C0008',
+            'id_user'=> '2019F0002',
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
